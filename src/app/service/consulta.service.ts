84929133d76f246a9a60103751a32e0717dae03b
Berni <bernalpas@gmail.com>
2022-11-14 16:15:45 -0300

    Deploy cuatro

--- src/app/service/consulta.service.ts
+++ src/app/service/consulta.service.ts
@@ -11,17 +11,17 @@ import { Consultas } from '../models/consultas';
 })
 export class ConsultaService {
 
-  //URL: string = 'http://localhost:3000/'
+ // URL: string = "https://app-node-ng.herokuapp.com/api"
 
   //URL: string = 'https://app-node-ng.herokuapp.com/api'
   
-  URL = environment.URL;
+ // URL = environment.URL;
 
   constructor(private http: HttpClient) { }
 
 
   guardarConsulta(consulta: Consultas): Observable<any> {
-    return this.http.post(this.URL, consulta)
+    return this.http.post("https://app-node-ng.herokuapp.com/api", consulta)
   }
 
 
