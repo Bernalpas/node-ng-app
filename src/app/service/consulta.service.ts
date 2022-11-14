ce4fc276a195efa0e46dce9550e19e834786dd12
Berni <bernalpas@gmail.com>
2022-11-14 15:40:14 -0300

    Deploy de node-ng app

---
+++ src/app/service/consulta.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Consultas } from '../models/consultas';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsultaService {
+
+  //URL: string = 'http://localhost:3000/'
+
+  URL: string = 'https://app-node-ng.herokuapp.com/api'
+  
+  //URL = environment.URL;
+
+  constructor(private http: HttpClient) { }
+
+
+  guardarConsulta(consulta: Consultas): Observable<any> {
+    return this.http.post(this.URL, consulta)
+  }
+
+
+}
